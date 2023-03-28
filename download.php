04b7e39175154d27e5cc58bbec5d3303e60ee8c0
markluizz <116421208+markluizz@users.noreply.github.com>
2023-03-28 11:28:13 +0400

    Add files via upload

---
+++ download.php
@@ -0,0 +1,33 @@
+ <?php
+$url = $_GET[url];
+
+$url = "https://dl.mylikelo.com/wp-json/aio-dl/api/?key=55a8ba3de9a71e7ef92914715b0c1f33c5cc9a86497a345558d692907d280380&url=$url";
+$json = file_get_contents($url);
+$json_data = json_decode($json, true);
+echo '<h2>';
+echo $json_data["title"];
+echo '</h2>';
+echo '<img src="'.$json_data["thumbnail"].'" alt='.$json_data["title"].'" style="width:40%;height:40%;" class="responsive">';
+echo '<br>';
+$events = $json_data['medias'];//events array
+
+
+    echo '<table class="table table-hover">';
+echo '<tr>';
+echo '<th>Quality</th>';
+echo '<th>Size</th>';
+echo '<th>Format</th>';
+echo '<th>Download</th>';
+echo '</tr>';
+foreach($events as $key=>$value)
+{
+     echo '<tr><td>' . $value['quality'] . '</td><td>' . $value['formattedSize'] . '</td><td>' . $value['extension'] . '</td><td><button type="button" class="btn btn-danger"><a href="' . $value['url'] . '">Download</a></button</td></tr>';
+}
+echo '</tr>';
+echo '</table>';
+
+
+  
+
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<form id="login-form" method="post" action="/download.php">                            
+<input class="form-control" type="url" name="url" placeholder="Enter URL Link: https://www.pinterest.com/pin/64518917981657198247/" aria-label="Search"><br><br>
+<div style="text-align: center;">
+<button class="btn btn-danger" type="submit">Download</button>
+</div>
+</form>
